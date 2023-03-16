8faa8b7b0153ef6b80232551062e931748ec6373
Mert Ernaz <rnzappdroid@gmail.com>
2023-03-16 22:58:49 +0300

    first type

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            byte a=2;
+            sbyte b=10;
+            short c=11;
+
+            int x=a+b+c;
+            long y=x;
+            double z=y;
+
+            Console.WriteLine(x+"/"+y+"/"+z);
+
+
+
+            string s="merhaba";
+            char t='c';
+            object o = s+t;
+            Console.WriteLine(o);
+
+
+            float f=5.6f;
+            byte bb=(byte)f;
+            Console.WriteLine(f+"//"+bb);
+
+        }
+    }
+}
